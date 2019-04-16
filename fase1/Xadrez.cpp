66a2f3bf9ea277de3dddf450a1017540fece313a
Adrian Todt <adriantodt@outlook.com>
2019-04-16 19:03:08 +0000

    código decente

--- fase1/Xadrez.cpp
+++ fase1/Xadrez.cpp
@@ -1,32 +1,80 @@
+#include<iostream>
+
+using namespace std;
+
 class Jogo {
 };
 
 class Jogador {
 };
 
-class Tabuleiro {
+class Peca {
+public:
+    void desenha();
+    
+    bool checaMovimento(int linhaOrigem, int colunaOrigem, int linhaDestino, int colunaDestino);
+
 };
 
 class Posicao {
+public:
+    Peca peca;
 };
 
-class Peca {
+class Tabuleiro {
+public:
+    Posicao posicoes[8][8];
+
+    void desenha() {
+        cout << "  |1 2 3 4 5 6 7 8" << endl << "--+---------------" << endl;
+
+        for (int y = 7; y >= 0; y--) {
+            cout << (char)('A' + y) << " |";
+            for (int x = 0; x < 8; x++) {
+                Posicao p = posicoes[x][y];
+                if (&(p.peca) != nullptr) {
+                    p.peca.desenha();
+                } else {
+                    cout << ((x + y) % 2 == 0) ? "•" : "+";
+                }
+                cout << " ";
+            }
+            cout << endl;
+        }
+    }
+    
+    bool checaMovimento(int linhaOrigem, int colunaOrigem, int linhaDestino, int colunaDestino) {
+        if (linhaDestino < 0 || colunaDestino < 0 || linhaDestino > 7 || colunaDestino > 7) return false;
+        
+        Posicao p = posicoes[x][y];
+        if (&(p.peca) != nullptr) {
+            return false;
+        } else {
+            return p.peca.checaMovimento(linhaOrigem, colunaOrigem, linhaDestino, colunaDestino);
+        }
+    }
 };
 
 class Rei {
 };
 
-class Dama : public Peca {
+class Dama {
 };
 
-class Cavalo : public Peca {
+class Cavalo {
 };
 
-class Bispo : public Peca {
+class Bispo {
 };
 
-class Torre : public Peca {
+class Torre {
 };
 
-class Peao : public Peca {
+class Peao {
 };
+
+int main() {
+    Tabuleiro tabuleiro;
+    tabuleiro.desenha();
+    return 0;
+}
